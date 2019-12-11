e49362d9463884ce50d27750431c7f8b07738bdf
Evan Slaton <evanslaton@gmail.com>
2019-12-11 08:50:32 -0800

    Added comments:

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 // creates the connection between the server and the database
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
+const database = new pg.Client(process.env.DATABASE_URL);
+database.connect();
 
 const app = express();
 const PORT = 3000;
@@ -29,7 +29,10 @@ app.get('/', greeting);
 app.get('/recipes', getRecipes);
 app.post('/save', saveRecipe);
 
-
+// request: all the 'stuff' we're sending to the server
+// response: all the 'stuff' we're sending back to the user
+// client: who is making the request
+// server: who is responding to the request
 function greeting(request, response) {
     response.send("<h1> A+ Best Team USA </h1><ol><li>This is a list</li></ol>");
 }
@@ -42,18 +45,18 @@ function getRecipes(request, response) {
       const data = JSON.parse(result.text);
       response.send(data.results);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
 function saveRecipe(request, response) {
   console.log(request.body);
   const SQL = `INSERT INTO recipes (title, url, ingredients) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING RETURNING id;`;
   const values = [request.body.title, request.body.href, request.body.ingredients];
-  client.query(SQL, values)
+  database.query(SQL, values)
     .then(result => {
       response.send({"info" : result.rows[0].id});
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
 // Starts server, always at end of file
