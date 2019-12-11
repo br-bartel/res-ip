375cc49b5a02ae48471c09377547453f3ef34f2e
Evan Slaton <evanslaton@gmail.com>
2019-12-11 08:54:57 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS recipes (
   id SERIAL PRIMARY KEY,
   title TEXT,
-  image_url TEXT,
+  url TEXT,
   ingredients TEXT
 );
 
-INSERT INTO recipes (title, image_url, ingredients) VALUES ('Vegetable-Pasta Oven Omelet', 'http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=520763', 'tomato, onions, red pepper, garlic, olive oil, zucchini, cream cheese, vermicelli, eggs, parmesan cheese, milk, italian seasoning, salt, black pepper');
+INSERT INTO recipes (title, url, ingredients) VALUES ('Vegetable-Pasta Oven Omelet', 'http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=520763', 'tomato, onions, red pepper, garlic, olive oil, zucchini, cream cheese, pasta, eggs, italian seasoning, salt, black pepper');
