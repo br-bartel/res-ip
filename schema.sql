28eb340181bd3412b583b7d7c68540487e391e72
xobre <mua.breana@gmail.com>
2019-12-18 15:48:55 -0800

    readme

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS recipes (
-  id SERIAL PRIMARY KEY,
-  title TEXT,
-  url TEXT,
-  ingredients TEXT
-);
-
-INSERT INTO recipes (title, url, ingredients) VALUES ('Vegetable-Pasta Oven Omelet', 'http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=520763', 'tomato, onions, red pepper, garlic, olive oil, zucchini, cream cheese, pasta, eggs, italian seasoning, salt, black pepper');
